3d4e101a2057eb09b4d29b6679a6edde0e4d08c5
Russell Cloak <russell.cloak@reachlocal.com>
2014-08-07 14:26:05 -0700

    Remove a todo for CqlArgumentError
    This will instead be tracked in a github issue.

--- lib/cql.rb
+++ lib/cql.rb
@@ -17,9 +17,6 @@ module Cql
   Io = Ione::Io
 end
 
-# TODO: Change the base class for InvalidUuidError to CqlError during the
-# next major release - then remove the CqlArgumentError base class because it
-# is only a placeholder for organizational purposes.
 CqlArgumentError = Class.new(ArgumentError)
 InvalidUuidError = Class.new(CqlArgumentError)
 
